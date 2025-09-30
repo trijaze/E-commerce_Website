5f663fc7418eddf390cdf02c6c7dd9d12a0cd485
trijaze <jaze112official@gmail.com>
2025-09-30 11:21:28 +0700

    update_diagram

--- bachhoa-frontend/src/pages/Profile.tsx
+++ bachhoa-frontend/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 // src/pages/Profile.tsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { authApi } from '../api/authApi';
 
 export default function Profile() {
