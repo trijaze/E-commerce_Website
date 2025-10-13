6c0f277f6bc51d0f49e79befb0c40aee9c266bcf
thinhdata05 <npthinhd300605@gmail.com>
2025-10-13 19:12:40 +0700

    fix JsonUtil

--- bachhoa-backend/src/main/java/vn/bachhoa/util/JsonUtil.java
+++ bachhoa-backend/src/main/java/vn/bachhoa/util/JsonUtil.java
@@ -13,7 +13,14 @@ public class JsonUtil {
         MAPPER.findAndRegisterModules();
         MAPPER.writeValue(resp.getOutputStream(), body);
     }
+
     public static void ok(HttpServletResponse resp, Object body) throws IOException {
         writeJson(resp, body, HttpServletResponse.SC_OK);
+    }
+
+    // Thêm method fromJson để parse JSON string thành object
+    public static <T> T fromJson(String jsonStr, Class<T> clazz) throws IOException {
+        MAPPER.findAndRegisterModules();
+        return MAPPER.readValue(jsonStr, clazz);
     }
 }
